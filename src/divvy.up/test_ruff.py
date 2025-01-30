701483968dc0472fcc83190bdbfc30768aed3850
Laurent Lanteigne <lanteignel@uchicago.edu>
2025-01-29 22:24:56 -0600

    Removed Ruff test.

--- src/divvy.up/test_ruff.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-
-test_dictionary = {"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123}
-
-test
-test
-test
