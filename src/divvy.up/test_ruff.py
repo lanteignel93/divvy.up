dbe0d5561d91fda398c4418242ee3c560c21577e
Laurent Lanteigne <lanteignel@uchicago.edu>
2025-01-29 21:49:36 -0600

    Test ruff.

---
+++ src/divvy.up/test_ruff.py
@@ -0,0 +1,5 @@
+import numpy as np 
+
+test_dictionary = {"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123,"vdfsdfsdfs" : 123123}
+
+test
